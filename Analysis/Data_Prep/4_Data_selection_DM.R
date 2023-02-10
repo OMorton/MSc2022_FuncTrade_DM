f150e6271afdcd1a92b6abf1961820eb33b378e4
Oscar Morton <oscarmorton94@gmail.com>
2023-02-10 17:54:41 +0000

    Data selection done

---
+++ Analysis/Data_Prep/4_Data_selection_DM.R
@@ -0,0 +1,15 @@
+# setwd("G:/My Drive/TUoS/Teaching/Masters/2022/Dom_Meeks/Project")
+.libPaths("C:/Packages")
+
+library(tidyverse)
+
+CITESFull_ER <- data.table::fread("Data/3_CITES_Vert_dat_ER.csv") %>% select(-V1)
+CITESFull_IR <- data.table::fread("Data/3_CITES_Vert_dat_IR.csv") %>% select(-V1)
+
+## Focus on just the reptile and bird trade (but include wild and captive/live and not live)
+## ER
+Reptile_CITES_Data <- CITESFull_ER %>% filter(Class == "Reptilia")
+Aves_CITES_Data <- CITESFull_ER %>% filter(Class == "Aves")
+
+write.csv(Reptile_CITES_Data, "Data/4_DM_Rept_ER.csv", na = "")
+write.csv(Aves_CITES_Data, "Data/4_DM_Aves_ER.csv", na = "")
